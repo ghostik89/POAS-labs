1cf6a8c4ab26330bab6886c4722722b759695655
ghostik89 <taras320@mail.ru>
2019-04-07 21:27:30 +0400

    Код был "причесан"

--- MyLabs/Laba005.cpp
+++ MyLabs/Laba005.cpp
@@ -6,89 +6,80 @@
 /*
 	Функция для выявления дублей в массиве без проверки массива на корректность
 */
-void FindEqual(const int arr[], const int N, int** ptrEqualArr, int& colums, int& rows){
-	//bool CorrectData = true;
-	////проверка N на принадлежность заданному диапозону
-	//if (N > 20 || N < 2) {
-	//	CorrectData = false;
-	//}
-	////проверка N на принадлежность заданному диапозону
-	//if (N > 20 || N < 2) {
-	//	CorrectData = false;
-	//}
+void FindDuplicate(const int arr[], const int N, int** ptrEqualArr, int& colums, int& rows){
+	
+		bool ItWrite;//вспомогательная переменная для расположения элементов в одной строчке
+		bool CheckedElement;//переменная для проверки были элемент ранее задействован в цикле
+		bool NotFoundDuplicate;//считается, что изначально в массиве нет дублей
+		int MaxColumns;//необходимо для нахождения максимального значения колонок в массиве, чтобы использовать это при печати
 
-	////считывание массива
-	//for (int i = 0; i < N; i++) {
-	//	//проверка элемента на принадлежность заданному диапозону
-	//	if (arr[i] < -1000 || arr[i] > 1000) {
-	//		CorrectData = false;
-	//	}
-	//}
-
-	////считывание массива
-	//for (int i = 0; i < N; i++) {
-	//	//проверка элемента на принадлежность заданному диапозону
-	//	if (arr[i] < -1000 || arr[i] > 1000) {
-	//		CorrectData = false;
-	//	}
-	//}
-
-	//bool NotEqual = true;//считается, что изначально в массиве нет дублей
-	//if (CorrectData) {
-		bool It_Write = false; //вспомогательная переменная для печати в одной строке
-		bool CheckedElement = false; //переменная для проверки были элемент ранее задействован в цикле
-		bool NotEqual = true;//считается, что изначально в массиве нет дублей
-		int maxcolums = 0;
-
+		//Инициализация значений для работы с ними
+		ItWrite = false;
+		CheckedElement = false;
+		NotFoundDuplicate = true;
+		MaxColumns = 0;
 		colums = 0;
 		rows = 0;
+
+		//поиск дубликатов в массиве
 		for (int i = 0; i < N - 1; i++) {
-			//поиск дублей
+
+			//поиск раннее проверенной переменной
 			for (int k = i - 1; k >= 0; k--) {
 				if (arr[k] == arr[i])
 					CheckedElement = true;
 			}
+
 			//если элемент ранее не использовался
 			if (!CheckedElement) {
 				for (int j = i + 1; j < N; j++) {
-					//если элемент arr[i] был напечатан впервые
+					//если элемент индекс arr[i] нету в массиве
 					//и не рассматривался ранее
-					if (arr[j] == arr[i] && !It_Write) {
+					if (arr[j] == arr[i] && !ItWrite) {
 						//printf("%d %d ", i, j);
+						//присовить значения
 						ptrEqualArr[rows][colums++] = i;
 						ptrEqualArr[rows][colums++] = j;
-						NotEqual = false;
-						It_Write = true;
+
+						//обозначить, что решения есть, и индекс элемента arr[i] есть в массиве
+						NotFoundDuplicate = false;
+						ItWrite = true;
 					}
 					//если у элемента arr[i] ранее были дубли
-					else if (arr[j] == arr[i] && It_Write) {
-						//printf("%d ", j);
+					else if (arr[j] == arr[i] && ItWrite) {
 						ptrEqualArr[rows][colums++] = j;
 					}
 				}
 			}
 
-			//если у элемента arr[i] были дубли и он не был ранее напечатан
-			if (It_Write) {
+			//если у элемента arr[i] были дубли и он не был ранее в массиве
+			if (ItWrite) {
 				rows++;
-				//colums = 0;
-				if (maxcolums < colums) {
-					maxcolums = colums;
+				//сохранить количество колонок для последующей печати
+				if (MaxColumns < colums) {
+					MaxColumns = colums;
 				}
+				//сбросить значение колонок
 				colums = 0;
 			}
-			//printf("\n");
-			It_Write = false;//сброс индикатора напечатанного элемента
+
+			//сброс значений 
+			ItWrite = false;
 			CheckedElement = false;
 		}
-		colums = maxcolums;
-	//}
+		colums = MaxColumns;
+
 	//дубли не найдены, решения нет
-	if (NotEqual)
+	if (NotFoundDuplicate)
 		ptrEqualArr[0][0] = -1001;
 
 }
 
+ /*
+	функция для нормализации значений в массиве,
+	т.е. избавить его от "мусорных значений"
+	int** arr - двумерный массив для нормализации
+ */
 void NormalizeAnswer(int** arr) {
 	for (int i = 0; i < 20; i++)
 	{
@@ -98,121 +89,140 @@ void NormalizeAnswer(int** arr) {
 		}
 	}
 }
-void print(int **M, int rows, int columns)
+
+/*
+	Функция для вывода двумерного массива
+	int **arr - двумерный массив, который нужно вывести
+	int rows - количество строчек массива
+	int columns - количество колонок в массиве
+*/
+void print(int **arr, int rows, int columns)
 {
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < columns; j++)
 		{
-			printf("%d\t", M[i][j]);
+			printf("%d\t", arr[i][j]);
 		}
 		printf("\n");
 	}
+
+	//вывод значения в случае, если нет решения
 	if (rows == 0 && columns == 0)
-		printf("%d\n", M[0][0]);
+		printf("%d\n", arr[0][0]);
 }
 
 //void CompareArrays(int** CurrentArray, int** ResultArray) {
 //	
 //}
 
-void CheckArray() {
-	int** EqualArr;
-	int colums, rows;
+/*
+	функция тестирования FindDuplicate()
+*/
+void UnitTests() {
+
+	int** EqualArr;//двумерный массив для хранения дубликатов
+	int colums, rows;//количество колонок и строчек массива
+
+	//инициализация и нормализация массива
 	EqualArr = new int*[20];
 	for (int i = 0; i < 20; i++)
 		EqualArr[i] = new int[20];
 	NormalizeAnswer(EqualArr);
-	int check1[10] = { 89, 70, 89, 1000, 1000, 56, 78, 89, 34, 34 };
-	//int check2[21] = {23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23};
-	int check3[5] = { 12, 1, 5, 6, 7};
-	//int check4[1] = { 12 };
-	//int check5[5] = { 1001, 2001, 56, 56, 45};
-	int check6[6] = { 1, 1, 2, 3, 4, 4};
-	int check7[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int check8[5] = { 1, 1, 1, 1, 1};
 
-	//придумать ответы
+	//исходные данные тестов
+	int check1[10] = { 89, 70, 89, 1000, 1000, 56, 78, 89, 34, 34 };
+	int check2[5] = { 12, 1, 5, 6, 7};
+	int check3[6] = { 1, 1, 2, 3, 4, 4};
+	int check4[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int check5[5] = { 1, 1, 1, 1, 1};
+
+	//правильные ответы на тесты
 	int answer1[3][3] = {
 		{0, 2, 7},
 		{3, 4, 0},
 		{8, 9, 0}
 	};
-	int InvalidAnswer = -1001;
-	int answer6[2][2] = { {0, 1}, {4, 5} };
-	int answer8[1][5] = { { 0, 1, 2, 3, 4} };
+	int NoSolusionAnswer = -1001;
+	int answer2[2][2] = { {0, 1}, {4, 5} };
+	int answer3[1][5] = { { 0, 1, 2, 3, 4} };
 	
-	//чекнуть и вывести
+	//тестирование
 
-	//test1
-	FindEqual(check1, 10, EqualArr, colums, rows);
+	//тест первый
+	FindDuplicate(check1, 10, EqualArr, colums, rows);
 	print(EqualArr, rows, colums);
-	bool IncorrectID = false;
-	//CompareArrays(EqualArr, answer1);
+	bool IncorrectID = false;//переменная для проверки на прохождение теста
 	for(int i = 0; i < 3; i++)
 		for(int j = 0; j < 3; j++)
-			if (answer1[i][j] != EqualArr[i][j]) {
+			if (answer1[i][j] != EqualArr[i][j]) {//если элементы не совпадают с эталонным
 				printf("Incorrect data in %d %d place\n", i, j);
 				IncorrectID = true;
 			}
+	//при прохождении теста вывести сообщение
 	if (!IncorrectID)
 		printf("Succuess!\n");
 	
+	//сброс данных для дальнейшего тестирования
 	NormalizeAnswer(EqualArr);
-	IncorrectID = false;
-
-	printf("===========================================\n");
-	//test2
-	FindEqual(check3, 5, EqualArr, colums, rows);
-	if (EqualArr[0][0] == InvalidAnswer)
-		printf("Succuess!\n");
+	printf("\n===========================================\n");
+	
+	//тест второй
+	FindDuplicate(check2, 5, EqualArr, colums, rows);
+	if (EqualArr[0][0] == NoSolusionAnswer)//если элементы не совпадают с эталонным
+		printf("Succuess! This test haven't solusion\n");
+	//при прохождении теста вывести сообщение
 	else
-		printf("Incorrect data. This test haven't solustion!\n");
+		printf("Incorrect data. This test haven't solusion!\n");
 
-	printf("===========================================\n");
+	//сброс значений
+	printf("\n===========================================\n");
 	NormalizeAnswer(EqualArr);
 	
-	//test3
-	FindEqual(check6, 6, EqualArr, colums, rows);
+	//тест третий
+	FindDuplicate(check3, 6, EqualArr, colums, rows);
 	print(EqualArr, rows, colums);
 	IncorrectID = false;
-	//CompareArrays(EqualArr, answer1);
 	for (int i = 0; i < 2; i++)
 		for (int j = 0; j < 2; j++)
-			if (answer6[i][j] != EqualArr[i][j]) {
+			if (answer2[i][j] != EqualArr[i][j]) {//если элементы не совпадают с эталонным
 				printf("Incorrect data in %d %d place\n", i, j);
 				IncorrectID = true;
 			}
+	//при прохождении теста вывести сообщение
 	if (!IncorrectID)
 		printf("Succuess!\n");
 	
-	printf("===========================================\n");
+	//сброс значений
+	printf("\n===========================================\n");
 	NormalizeAnswer(EqualArr);
 	
-	//test4
-	FindEqual(check7, 9, EqualArr, colums, rows);
-	if (EqualArr[0][0] == InvalidAnswer)
-		printf("Succuess!\n");
+	//тест четвертый
+	FindDuplicate(check4, 9, EqualArr, colums, rows);
+	if (EqualArr[0][0] == NoSolusionAnswer)//если элементы не совпадают с эталонным
+		printf("Succuess! This test haven't solusion\n");
+	//при прохождении теста вывести сообщение
 	else
 		printf("Incorrect data. This test haven't solustion!\n");
 
-	printf("===========================================\n");
+	printf("\n===========================================\n");
 	NormalizeAnswer(EqualArr);
 
-	//test 5
-	FindEqual(check8, 5, EqualArr, colums, rows);
+	//тест пятый
+	FindDuplicate(check5, 5, EqualArr, colums, rows);
 	print(EqualArr, rows, colums);
 	IncorrectID = false;
-	//CompareArrays(EqualArr, answer1);
 	for (int i = 0; i < 1; i++)
 		for (int j = 0; j < 5; j++)
-			if (answer8[i][j] != EqualArr[i][j]) {
+			if (answer3[i][j] != EqualArr[i][j]) {//если элементы не совпадают с эталонным
 				printf("Incorrect data in %d %d place\n", i, j);
 				IncorrectID = true;
 			}
+	//при прохождении теста вывести сообщение
 	if (!IncorrectID)
 		printf("Succuess!\n");
 
+	//удалить массив
 	for (int i = 0; i < 20; i++)
 		delete[] EqualArr[i];
 	delete[] EqualArr;
@@ -242,23 +252,30 @@ int main() {
 			return 0;
 		}
 	}
-	int** EqualArr;
-	int colums, rows;
+
+	int** EqualArr;//двумерный массив для записи индексов дубликатов
+	int colums, rows;//колонки и строки для вывода
+
+	//инициализация массива
 	EqualArr = new int*[20];
 	for (int i = 0; i < 20; i++)
 		EqualArr[i] = new int[20];
-
-	//заполнить массив значениями
-	FindEqual(arr, N, EqualArr, colums, rows);
-	if (EqualArr[0][0] == -1001)
+	
+	//поиск дубликатов
+	FindDuplicate(arr, N, EqualArr, colums, rows);
+	if (EqualArr[0][0] == -1001) //вывод в случае, если дубликатов нет
 		printf("no solustion!");
+	//вывести дубликаты
 	else
 		print(EqualArr, rows, colums);
-	CheckArray();
-	
+	printf("\n\n");
+
+	//удалить массив
 	for (int i = 0; i < 20; i++)
 		delete[] EqualArr[i];
 	delete[] EqualArr;
+
+	UnitTests();
 	
 	return 0;
 }
