14eda301b3dcf712222c907ef93c10304ac97117
@Lady_Proger <facktartar@gmail.com>
2019-06-11 19:52:54 +0400

    cds
    csd

---
+++ MyLabs/Source1.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cmath>
+#include <conio.h>
+
+num1 = 3
+num2 = 4
+
+int summ(int num1, int num2)
+{
+	int sum, num;
+	num = num1 - num2;
+	sum = num1 + num2;
+	return sum;
+}
+
+void main(void)
+{
+	int sum, num1, num2;
+	sum = summ(num1, num2);
+}
